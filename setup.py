cedcb1fc95ef5c058d157045355a73b6461d9c2a
Suvam Ray <suvamray2008@gmail.com>
2025-01-14 12:30:41 +0530

    First commit - Project folder setup using cookiecutter and git setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project will help in recommending top 3 products to each user viting the swiggy homepage',
+    author='Suvam Ray',
+    license='',
+)
